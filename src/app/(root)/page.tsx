418904cdc48cbfce296228f4fa8ffaaada6c7732
madhav <madhavendranaths@gmail.com>
2025-09-30 19:12:55 +0530

    created a homepage with widgets

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import TradingViewWidget from "@/components/TradingViewWidget";
+import { HEATMAP_WIDGET_CONFIG, MARKET_DATA_WIDGET_CONFIG, MARKET_OVERVIEW_WIDGET_CONFIG, TOP_STORIES_WIDGET_CONFIG } from "@/lib/constants";
+import React from "react";
 
 const Home = () => {
   return (
-    <div > Home </div>
-  )
-}
+    <div className="flex min-h-screen home-wrapper">
+      <section className="grid w-full gap-8 home-section">
+        <div className="md:col-span-1 xl:col-span-1">
+          <TradingViewWidget
+            title="Market Overview"
+            scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js"
+            config={MARKET_OVERVIEW_WIDGET_CONFIG}
+            className="custom-chart"
+          />
+        </div>
+        <div className="md:col-span-1 xl:col-span-2">
+          <TradingViewWidget
+            title="Stock Heatmap"
+            scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-stock-heatmap.js"
+            config={HEATMAP_WIDGET_CONFIG}
+            
+          />
+        </div>
+      </section>
+      <section className="grid w-full gap-8 home-section">
+        <div className="h-full md:col-span-1 xl:col-span-1">
+          <TradingViewWidget
+            title="Market Overview"
+            scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-timeline.js"
+            config={TOP_STORIES_WIDGET_CONFIG}
+            className="custom-chart"
+          />
+        </div>
+        <div className="h-full md:col-span-1 xl:col-span-2">
+          <TradingViewWidget
+            title="Stock Heatmap"
+            scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-market-quotes.js"
+            config={MARKET_DATA_WIDGET_CONFIG}
+          
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
